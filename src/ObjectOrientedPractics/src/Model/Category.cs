1a254d45bb02a8b6316f8c0f4647b4abc2920c52
akarmanoff <arseniy.karmanov@mail.ru>
2022-10-25 19:47:40 +0700

    Добавил перечисление Category.

---
+++ src/ObjectOrientedPractics/src/Model/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Перечисление категории товаров.
+    /// </summary>
+    public enum Category
+    {
+        Cars,
+        Sweets,
+        Furniture,
+        BuildingMaterials,
+        Electronics,
+        Food,
+        Education,
+        Medicine
+    }
+}
