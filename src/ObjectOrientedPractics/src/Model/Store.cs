ce3fde828309e478c06b883c3f2a1f5ed04793af
akarmanoff <arseniy.karmanov@mail.ru>
2022-12-07 21:55:35 +0700

    Реализовал ICloneable и IEquatable для класса Store.

--- src/ObjectOrientedPractics/src/Model/Store.cs
+++ src/ObjectOrientedPractics/src/Model/Store.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ObjectOrientedPractics.Model;
 
 /// <summary>
 /// Представляет объект - магазин, который хранит в себе списки <see cref="Item"/> и <see cref="Customer"/>.
 /// </summary>
-public class Store
+public class Store : ICloneable, IEquatable<Store>
 {
     /// <summary>
     /// Возвращает или задает список объектов типа <see cref="Item"/>
@@ -16,4 +17,53 @@ public class Store
     /// Возвращает или задает список объектов типа <see cref="Customer"/>.
     /// </summary>
     public List<Customer> Customers { get; set; } = new();
+
+    public object Clone()
+    {
+        var store = new Store();
+        foreach (var item in Items)
+        {
+            store.Items.Add((Item)item.Clone());
+        }
+
+        foreach (var customer in Customers)
+        {
+            store.Customers.Add((Customer)customer.Clone());
+        }
+
+        return store;
+    }
+
+    public bool Equals(Store other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Equals(Items, other.Items) && Equals(Customers, other.Customers);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == GetType() && Equals((Store)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return ((Items != null ? Items.GetHashCode() : 0) * 397) ^
+                   (Customers != null ? Customers.GetHashCode() : 0);
+        }
+    }
+
+    public static bool operator ==(Store left, Store right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Store left, Store right)
+    {
+        return !Equals(left, right);
+    }
 }
