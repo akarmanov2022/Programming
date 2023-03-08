a37ceb1613d64f4e566ac5f90bf0a588c3210de4
Arseniy Karmanov <arseniy.karmanov@mail.ru>
2023-03-08 09:39:22 +0700

    Добавил команды SaveCommand.cs и LoadCommand.cs.

---
+++ src/Contacts/ViewModels/LoadCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Contacts.ViewModels;
+
+public class LoadCommand : ICommand
+{
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        // throw new NotImplementedException();
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
