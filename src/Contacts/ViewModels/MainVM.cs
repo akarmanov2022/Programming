e4ceece33eac9bdcde22fb58bc7d13a62d2ae170
Arseniy Karmanov <arseniy.karmanov@mail.ru>
2023-03-08 09:38:39 +0700

    Добавил класс представления MainVM.cs.

---
+++ src/Contacts/ViewModels/MainVM.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Contacts.Models;
+
+namespace Contacts.ViewModels;
+
+/// <summary>
+/// Привет
+/// </summary>
+public sealed class MainVM : INotifyPropertyChanged
+{
+    private string _name;
+
+    private string _phoneNumber;
+
+    private string _email;
+
+    public MainVM(Contact contact)
+    {
+        Contact = contact;
+    }
+
+    public Contact Contact { get; }
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (value == _name) return;
+            _name = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string PhoneNumber
+    {
+        get => _phoneNumber;
+        set
+        {
+            if (value == _phoneNumber) return;
+            _phoneNumber = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Email
+    {
+        get => _email;
+        set
+        {
+            if (value == _email) return;
+            _email = value;
+            OnPropertyChanged();
+        }
+    }
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
